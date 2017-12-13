bfee87558c329d25f23624915df30be0dd01dca3
yujipuada <yujipuada@yahoo.com>
2017-12-13 22:11:23 +0800

    added delete todo

--- list.js
+++ list.js
@@ -5,12 +5,11 @@ var input = prompt("What would you like to do?");
 while (input !== "quit") {
   // handle input
   if (input === "list") {
-    console.log(todos);
+    listTodos();
   } else if (input === "new") {
-    // ask for new todo
-    var newTodo = prompt("Enter new todo")
-    // add to todo's array
-    todos.push(newTodo);
+    addTodo();
+  } else if (input === "delete") {
+    deleteTodo();
   }
 
   // ask again for todo
@@ -18,3 +17,27 @@ while (input !== "quit") {
 }
 
 console.log("OK, you quit the app.");
+
+function listTodos() {
+  console.log("**********");
+  todos.forEach(function(todo,i) {
+    console.log(i + ": " + todo);
+  });
+  console.log("**********");
+}
+
+function addTodo() {
+  // ask for new todo
+  var newTodo = prompt("Enter new todo")
+  // add to todo's array
+  todos.push(newTodo);
+  console.log("Added Todo")
+}
+
+function deleteTodo() {
+  // ask for index of todo to be deleted
+  var index = prompt("Enter index of todo to delete");
+  // delete that todo
+  todos.splice(index, 1)
+  console.log("Deleted Todo")
+}
